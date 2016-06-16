27aed3abea6256ab1df2d8398618d93e3c30cb74
Gatoretrasado <josan1995@msn.com>
2016-06-16 08:41:21 +0200

    solo falta base datos 2.0

---
+++ app/src/main/java/com/example/josan/proyecto/Tarea.java
@@ -0,0 +1,31 @@
+package com.example.josan.proyecto;
+
+public class Tarea{
+
+    private String nombre;
+    private String hora;
+    private int categoria;
+
+    public Tarea(String nombre, String hora, int categoria){
+        this.nombre = nombre;
+        this.hora = hora;
+        this.categoria = categoria;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setHora(String hora){
+        this.hora = hora;
+    }
+
+    public void setCategoria(int categoria){
+        this.categoria=categoria;
+    }
+
+    public String getNombre(){return nombre;}
+    public String getHora(){return hora;}
+    public int getCategoria(){return categoria;}
+
+}
